bc3d128890d20d6841b3e9923d0d3b20cec4e3ef
AdamAlibraheem <shrineknight000@gmail.com>
2022-01-23 10:43:13 -0500

    completed schema tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,27 @@
 DROP TABLE IF EXISTS department;  
-DROP TABLE IF EXISTS role;
-DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
 
 CREATE TABLE department(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+    d_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role(
+CREATE TABLE roles(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary: DECIMAL(10, 2) NOT NULL,
-    department_id: INTEGER NOT NULL
-)
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INTEGER NOT NULL
+);
 
-CREATE TABLE employee(
+CREATE TABLE employees(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    CONSTRAINT role_id FOREIGN KEY () REFERENCES role(id) ON DELETE CASCADE--to hold reference to employee role
-    CONSTRAINT manager_id FOREIGN KEY
-    --INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
+    role_id INTEGER,
+    CONSTRAINT manager_id FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
+);
+   
 
         
 
